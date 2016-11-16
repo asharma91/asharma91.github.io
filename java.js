0ecc6eb23c464973dd12ebc2cf906eeec7142e3b
asharma91 <aditi.sharma311@gmail.com>
2016-11-15 16:50:56 -0800

    Update java.js

--- java.js
+++ java.js
@@ -1,73 +1,98 @@
+
+
 var map = L.map('mapid').setView([40.719190, -73.996589], 13);
+
 var CartoDBTiles = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',{
   attribution: 'Map Data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> Contributors, Map Tiles &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
 });
+
+
 map.addLayer(CartoDBTiles);
 
-var marker = L.marker([40.719189,-73.996589]).addTo(map)
-
-marker.bindPopup("<b>G.Rider</b><br><b>Favorite Food:</b> Fresh Souls</br><b>Favorite Song:</b> Free Bird</br><b>Favorite Movie:</b> The Jerk</br><b>Favorite Hobby:</b> Poppin-Wheelies!").openPopup();
-
-var featureClick = function (feature, layer) {
-	layer.bindPopup(
-"<strong>Name:</strong> " + feature.properties.Myname + "<br /><strong>Favorite Food:</strong> " + feature.properties.FavFood + "<br /><strong>Favorite Song:</strong> " + feature.properties.FavSong + "<br /><strong>Favorite Movie:</strong> " + feature.properties.FavMovie + "<br /><strong>Favorite Hobby:</strong> " + feature.properties.FavHobby)
+function fillColor(d) {
+    return d > 500000 ? '#006d2c' :
+           d > 250000 ? '#31a354' :
+           d > 100000 ? '#74c476' :
+           d > 50000  ? '#a1d99b' :
+           d > 10000  ? '#c7e9c0' :
+                        '#edf8e9';
 }
-var myPoints = L.geoJson(geojsonFeature, {
-    onEachFeature: featureClick
-}).addTo(map);
-//var marker2 = L.marker([40.719189, -73.996589]).addTo(map)
-//marker.bindPopup("<b>G.Rider</b><br><b>Favorite Food:</b> Fresh Souls</br><b>Favorite Song:</b> Free Bird</br><b>Favorite Movie:</b> The Jerk</br><b>Favorite Hobby:</b> Poppin-Wheelies!").openPopup();
 
-var unemploymentStyle = function (feature){
-    var value = feature.properties.UnempRate;
-    var fillColor = null;
-    if(value >= 0 && value <=0.025){
-		fillColor = "#4dac26";
-    }
-    if(value >0.02 && value <=0.05){
-        fillColor = "#a6d96a";
-    }
-    if(value >0.05 && value<=0.075){
-    	fillColor = "#fc8d59";
-    }
-    if(value > 0.075 && value <=0.10){
-    	fillColor = "#e34a33";
-    }
-    if(value > 0.10 && value <=0.125) { 
-		fillColor = "#e6550d";
-    }
-    if(value > 0.125) { 
-		fillColor = "#b30000";
-    }
+function radius(d) {
+    return d > 9000 ? 20 :
+           d > 7000 ? 12 :
+           d > 5000 ? 8  :
+           d > 3000 ? 6  :
+           d > 1000 ? 4  :
+                      2 ;
+}
 
-    var style = {
-        weight: 1,
-        opacity: .1,
-        color: 'white',
-        fillOpacity: 0.75,
-        fillColor: fillColor
+var checkCashingStyle = function (feature, latlng){
+    var checkCashingMarker = L.circleMarker(latlng, {
+        stroke: false,
+        fillColor: fillColor(feature.properties.amount),
+        fillOpacity: 1,
+        radius: radius(feature.properties.customers)
+    });
+    
+    return checkCashingMarker;
+    
+}
+
+
+var checkCashingInteraction = function(feature,layer){
+
+
+    var highlight = {
+        stroke: true,
+        color: '#ffffff', 
+        weight: 3,
+        opacity: 1,
     };
 
-    return style;
+    var clickHighlight = {
+        stroke: true,
+        color: '#f0ff00', 
+        weight: 3,
+        opacity: 1,
+    };
+
+    var noHighlight = {
+        stroke: false,
+    };
+
+
+layer.on('mouseover', function(e) {
+        layer.setStyle(highlight);
+        if (!L.Browser.ie && !L.Browser.opera) {
+            layer.bringToFront();
+        }
+        
+    });
+
+
+layer.on('mouseout', function(e) {
+        layer.setStyle(noHighlight); 
+                        
+    });
+
+
+layer.on("click",function(e){
+        
+        layer.bindPopup('<div class="popupStyle"><h3>' + feature.properties.name + '</h3><p>'+ feature.properties.address + '<br /><strong>Amount:</strong> $' + feature.properties.amount + '<br /><strong>Customers:</strong> ' + feature.properties.customers + '</p></div>').openPopup();
+        layer.setStyle(clickHighlight); 
+    });
+
+
 }
-var unemploymentClick = function (feature, layer) {
-	var percent = feature.properties.UnempRate * 100;
-	percent = percent.toFixed(0);
-	// let's bind some feature properties to a pop up
-	layer.bindPopup("<strong>Neighborhood:</strong> " + feature.properties.NYC_NEIG + "<br /><strong>Unemployment Rate: </strong>" + percent + "%");
-}
-var neighborhoodsGeoJSON = L.geoJson(neighborhoods, {
-    style: unemploymentStyle,
-    onEachFeature: unemploymentClick
-}).addTo(map);
 
-var baseMaps = {
-    "CartoDB": CartoDBTiles,
-};
+var checkCashingCustomStuff = L.geoJson(null, {
+    pointToLayer: checkCashingStyle,
+    onEachFeature: checkCashingInteraction
+});
 
-var overlayMaps = {
-    "Zombie Map": myPoints,
-    "Unemployment Map": neighborhoodsGeoJSON
-};
 
-L.control.layers(baseMaps, overlayMaps).addTo(map);
+var checkCashingLayer = omnivore.csv('CheckCashing.csv', null, checkCashingCustomStuff).addTo(map);
+
+
+
